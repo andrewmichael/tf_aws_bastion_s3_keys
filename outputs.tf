79c8dcc3ba188804d5cef1d325fbc5826576a808
A Bourne <andrewmbourne@gmail.com>
2018-03-05 09:46:19 +0000

    output the asg name and id

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,11 @@ output "ssh_user" {
 output "security_group_id" {
   value = "${aws_security_group.bastion.id}"
 }
+
+output "asg_name" {
+  value = "${aws_autoscaling_group.bastion.name}"
+}
+
+output "asg_name_id" {
+  value = "${aws_autoscaling_group.bastion.id}"
+}
